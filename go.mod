c07e8935f128a442c93f53910f65dec2c605aeb3
akramov1ch <shaxbozakramovic@gmail.com>
2024-05-10 08:08:09 +0500

    origin

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module 32Homework
 
 go 1.22.2
 
-require github.com/lib/pq v1.10.9
+require (
+	github.com/lib/pq v1.10.9
+	golang.org/x/sync v0.7.0
+)
--- main.go
+++ main.go
@@ -6,10 +6,9 @@ import (
     "fmt"
     "log"
     "math/rand"
-    "sync"
     "time"
-
-    _ "github.com/lib/pq" 
+    _ "github.com/lib/pq"
+    "golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -18,48 +17,34 @@ func main() {
         log.Fatalf("Xatolik: %v", err)
     }
     defer db.Close()
-
     ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
     defer cancel()
-
-    var wg sync.WaitGroup
-    wg.Add(3)
-
-    go func() {
-        defer wg.Done()
+    g, ctx := errgroup.WithContext(ctx)
+    g.Go(func() error {
         _, err := db.ExecContext(ctx, "INSERT INTO large_dataset (data) VALUES ($1)", rand.Int())
-        if err != nil {
-            log.Printf("Insert xatoligi: %v", err)
-        }
-    }()
-
-    go func() {
-        defer wg.Done()
+        return err
+    })
+    g.Go(func() error {
         rows, err := db.QueryContext(ctx, "SELECT data FROM large_dataset")
         if err != nil {
-            log.Printf("Select xatoligi: %v", err)
-            return
+            return err
         }
         defer rows.Close()
-
         for rows.Next() {
             var data int
             err = rows.Scan(&data)
             if err != nil {
-                log.Printf("Scan xatoligi: %v", err)
-                return
+                return err
             }
             fmt.Println("Data:", data)
         }
-    }()
-
-    go func() {
-        defer wg.Done()
+        return nil
+    })
+    g.Go(func() error {
         _, err := db.ExecContext(ctx, "UPDATE large_dataset SET data = $1 WHERE data = $2", rand.Int(), rand.Int())
-        if err != nil {
-            log.Printf("Update xatoligi: %v", err)
-        }
-    }()
-
-    wg.Wait()
+        return err
+    })
+    if err := g.Wait(); err != nil {
+        log.Fatal(err)
+    }
 }
